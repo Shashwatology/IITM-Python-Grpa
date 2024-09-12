3ef70c8dfb58c953ee019d894d5e175b21316da2
shashwatology <shashwatology.s@gmail.com>
2024-09-12 08:20:09 +0530

    Week 2 Grpa

---
+++ Grpa Week 2/question1.py
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        body{
+            background: #d5cb3b;
+            align-items: center;
+            font-family: 'Times New Roman', Times, serif;
+            font-size: small;
+
+        }
+        .div{
+            color: aqua;
+            background: #000;
+            
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Learning Form Element </title>
+</head>
+    <body>
+        <for">
+            <div>
+                <label for="username">Enter your username: </label>
+                <input type="text" placeholder="username" id="username">
+            </div>
+            <div>
+                <label for="password">Enter your password: </label>
+                <input type="password" placeholder="type your password here" id="password">
+            </div>
+            <div>
+                <label for="number">Enter your number: </label>
+                <input type="number" placeholder="Provide contact detail's" id="number">
+            </div>
+            <div>
+                <button>Submit</button>
+            </div>
+            <div>
+                <input type="text" inputmode="email" inputmode="number" placeholder="username, number, email">
+            </div>
+            <button ></button>
+        </form>
+    </body>
+
+    <script>
+
+    </script>
+</html>
